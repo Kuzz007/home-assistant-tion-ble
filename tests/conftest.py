531e91ae149af9ace2531da7313ef18193431798
Kuzz007 <kuzzrus@gmail.com>
2026-08-01 00:48:23 +0300

    Изолировать BLE-тесты от USB-зависимостей

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,14 @@ from types import ModuleType
 PACKAGE_NAME = "custom_components.tion_ble"
 PACKAGE_PATH = Path(__file__).parents[1] / "custom_components" / "tion_ble"
 
+# Unit tests do not use Home Assistant's scanner implementation. Stub it so
+# importing the client does not pull optional USB watcher dependencies into the
+# isolated test environment.
+sys.modules.setdefault(
+    "homeassistant.components.bluetooth",
+    ModuleType("homeassistant.components.bluetooth"),
+)
+
 package = ModuleType(PACKAGE_NAME)
 package.__path__ = [str(PACKAGE_PATH)]
 package.TionBleConfigEntry = object
